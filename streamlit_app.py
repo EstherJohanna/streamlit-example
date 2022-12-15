4aa248e27b5a35b984afc77beb59ae6606fe797d
EstherJohanna <estjohbos@gmail.com>
2022-12-15 20:30:18 +0100

    blabla

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write("i'm a professor bitches")
 
-answer = st.selectbox('Pick one', ['cats', 'dogs'])
+answer = st.selectbox('Pick one', ['cat', 'dog'])
 
 
 password = st.text_input('Password')

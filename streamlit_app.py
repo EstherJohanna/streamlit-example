df0032b918d696a3e47839adf16c510639071bfd
bosc_es <estjohbos@gmail.com>
2022-12-31 12:47:42 +0100

    alle fragen drin

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,24 @@
 #run on website: commit and push: https://estherjohanna-streamlit-example-streamlit-app-egl5jh.streamlit.app/
 import streamlit as st
 
-st.write("R stürzt immer ab, wenn ich es benutze")
+st.write("R-Angst Fragebogen")
 
-answer = st.select_slider('R stürzt immer ab, wenn ich es benutze', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
+answer1 = st.select_slider('R stürzt immer ab, wenn ich es benutze', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
+
+answer2 = st.select_slider('Ich kann nicht schlafen, wenn ich an Eigenvektoren denke', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
+
+answer3 = st.select_slider('Standardabweichungen begeistern mich', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
+
+answer4 = st.select_slider('Ich habe wenig Erfahrung mit Computern', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
+
+answer5 = st.select_slider('Ich befürchte, dass ich irreparable Schäden verursachen werde wegen meiner Inkompetenz im Umgang mit Computern', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
+
+answer6 = st.select_slider('Ich wache unter meiner Bettdecke auf und denke, dass ich unter einer Normalverteilung gefangen bin', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
+
+answer7 = st.select_slider('Alle Computer hassen mich', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
 
 
-password = st.text_input('Password')
+password = st.text_input('efa')
 
-if password == "bitches":
-    st.write(f"you're type is: {answer}")
+if password == "efa":
+    st.write(f"your type is: {answer}")

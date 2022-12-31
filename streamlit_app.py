9ceb9d869afa366b0799dbeeefaabf6c1eea68e3
bosc_es <estjohbos@gmail.com>
2022-12-31 12:31:18 +0100

    richtige frage rein

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
+#run locally:  streamlit run .\streamlit_app.py
+#run on website: commit and push: https://estherjohanna-streamlit-example-streamlit-app-egl5jh.streamlit.app/
 import streamlit as st
 
-st.write("i'm a professor bitches")
+st.write("R stürzt immer ab, wenn ich es benutze")
 
-answer = st.selectbox('Pick one', ['cats', 'dogs'])
+answer = st.select_slider('R stürzt immer ab, wenn ich es benutze', ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu'])
 
 
 password = st.text_input('Password')

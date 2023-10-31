7d5db56e15695f7da3879cee29d66b51b8458f4c
bosc_es <estjohbos@gmail.com>
2023-10-31 14:34:31 +0100

    changed to Vorlesungsteilnahme

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,55 +2,41 @@
 #run on website: commit and push: https://estherjohanna-streamlit-example-streamlit-app-egl5jh.streamlit.app/
 import streamlit as st
 
-st.title("Fragebogen zur R-Angst1")
+st.title("Fragebogen zur Vorlesungsteilnahme")
 
 showResult = False
 
-listOfAnswers = ['123Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu']
-answer1 = st.radio('R stürzt immer ab, wenn ich es benutze.', listOfAnswers)
+listOfAnswers = ['Ich stimme gar nicht zu', 'Ich stimme nicht zu', 'Weder - noch', 'Ich stimme zu','Ich stimme voll und ganz zu']
+answer1 = st.radio('Ich bin ein Detektiv, der die Geheimnisse des menschlichen Verhaltens lüftet, wenn ich eine Vorlesung über Human Factors besuche.', listOfAnswers)
 index1 = listOfAnswers.index(answer1)
 
-answer2 = st.radio('Ich kann nicht schlafen, wenn ich an Eigenvektoren denke.', listOfAnswers)
+answer2 = st.radio('Meine Freunde würden mir ein ‚High Five‘ dafür geben, dass ich regelmäßig an Human Factors-Vorlesungen teilnehme.', listOfAnswers)
 index2 = listOfAnswers.index(answer2)
 
-answer3 = st.radio('Standardabweichungen begeistern mich.', listOfAnswers)
+answer3 = st.radio('Ich kann einen engen Zeitplan überwinden, um Zeit für den Besuch von Human Factors-Vorlesungen zu finden.', listOfAnswers)
 index3 = listOfAnswers.index(answer3)
 
-answer4 = st.radio('Ich habe wenig Erfahrung mit Computern.', listOfAnswers)
+answer4 = st.radio('Gäbe es für die Teilnahme an Human Factors-Vorlesungen „Smarty-Punkte" würde ich möglichst alle davon einsammeln.', listOfAnswers)
 index4 = listOfAnswers.index(answer4)
-
-answer5 = st.radio('Ich befürchte, dass ich irreparable Schäden verursachen werde wegen meiner Inkompetenz im Umgang mit Computern.', listOfAnswers)
-index5 = listOfAnswers.index(answer5)
-
-answer6 = st.radio('Ich wache unter meiner Bettdecke auf und denke, dass ich unter einer Normalverteilung gefangen bin.', listOfAnswers)
-index6 = listOfAnswers.index(answer6)
-
-answer7 = st.radio('Alle Computer hassen mich.', listOfAnswers)
-index7 = listOfAnswers.index(answer7)
 
 
 if st.button('Speichern'):
     st.write('Vielen Dank!')
 
 
-st.title("Ergebnis: Welcher R-Angst-Typ sind Sie?")
+st.title("Ergebnis: Wie gut passt die Theorie des geplanten Verhaltens auf Sie?")
 password = st.text_input('Bitte geben Sie das Passwort ein.')
 
 #typ ausrechnen
 
-computerangst = (index4 + index1 + index5 + index7) / 4
-statsangst = (index2 + (4-index3) + index6) / 3
+collectingchance = index4
 
-if (computerangst >= 2) and (statsangst >= 2):
-    answer = 'Sie haben sowohl Angst vor Computern, als auch vor Statistik! Therapievorschlag: Kommen Sie in meine Vorlesung und Übung!'
-elif (computerangst >= 2) and (statsangst < 2):
-    answer = 'Sie haben Angst vor Computern! Therapievorschlag: Kommen Sie in meine Übung!'
-elif (computerangst < 2) and (statsangst >= 2):
-    answer = 'Sie haben Angst vor Statistik! Therapievorschlag: Kommen Sie in meine Vorlesung!'
+if (collectingchance > 2):
+    answer = 'Sie werden die Smarties einsammeln!'
 else:
-    answer = 'Sie haben weder Angst vor Statistik, noch vor Computern! Wollen Sie eine Abschlussarbeit bei mir schreiben ;)?'
+    answer = 'Sie werden die Smarties nicht einsammeln!'
 
 
 
-if password == "HFU":
+if password == "HKA":
     st.write(f"Ergebnis: {answer}")
